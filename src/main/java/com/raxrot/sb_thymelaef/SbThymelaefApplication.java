6084e2cbab8866b77ccb9ad2d214c518011af7fe
Vlad Bulahov <dasistperfektosss@gmail.com>
2024-11-14 19:15:54 +0000

    init

---
+++ src/main/java/com/raxrot/sb_thymelaef/SbThymelaefApplication.java
@@ -0,0 +1,13 @@
+package com.raxrot.sb_thymelaef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbThymelaefApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbThymelaefApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/raxrot/sb_thymelaef/SbThymelaefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raxrot.sb_thymelaef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbThymelaefApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
